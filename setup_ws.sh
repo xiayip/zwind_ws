40ebed017d0fc5324180f567504384f91d73f7ad
xiayipeng <Patrick.xia@hotmail.com>
2025-09-18 16:02:16 +0800

    fix: update docker args to use correct home directory for SSH and add USB device access

--- setup_ws.sh
+++ setup_ws.sh
@@ -18,5 +18,5 @@ vcs import < dev.repos --skip-existing
 
 cd ./src/zwind_common/isaac_ros_common/scripts
 echo -e "CONFIG_IMAGE_KEY=ros2_humble.zwind\nCONFIG_DOCKER_SEARCH_DIRS=(../../../../docker/ ../docker)" > .isaac_ros_common-config
-echo -e "-v $HOME/.ssh:/root/.ssh:ro" > .isaac_ros_dev-dockerargs
+echo -e "-v $HOME/.ssh:/home/admin/.ssh:ro\n--device /dev/bus/usb" > .isaac_ros_dev-dockerargs
 # echo -e "-v /etc/nova/:/etc/nova/\n-v /opt/nvidia/nova/:/opt/nvidia/nova/\n-v /mnt/nova_ssd/recordings:/mnt/nova_ssd/recordings" > .isaac_ros_dev-dockerargs
