88d1a6e42d2dbadfa79b1c2c9b8a0524bfde2161
xiayipeng <yipeng.xia@hotmail.com>
2025-12-16 01:12:44 +0800

    fix: set txqueuelen for can0 and can1 in zwind_device_setup.sh

--- scripts/startup/zwind_device_setup.sh
+++ scripts/startup/zwind_device_setup.sh
@@ -7,8 +7,10 @@ sudo modprobe mttcan
 # can0
 sudo ip link set down can0
 sudo ip link set can0 type can bitrate 1000000
+sudo ip link set can0 txqueuelen 1000
 sudo ip link set up can0
 # can1
 sudo ip link set down can1
 sudo ip link set can1 type can bitrate 1000000
+sudo ip link set can1 txqueuelen 1000
 sudo ip link set up can1
